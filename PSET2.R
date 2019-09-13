df70fdc7414fbe168cc6a745ee3718c35d9045d5
CITADEL\Elmer <elm.s.camargo@gmail.com>
2019-09-13 13:58:31 -0700

    cleaned up R code

--- PSET2.R
+++ PSET2.R
@@ -1,12 +1,13 @@
 library(tidyverse)
-output_dir = "/output"
-wd = setwd(here::here("data"))
+
+rm(list=ls())
+wd = setwd("C:/Users/Elmer/Documents/R/Statistical Modeling/PSET2")
 
 #a
 #movies <- read.csv("data/movie_metadata.csv")
 
 #b
-movies <-read.csv("movie_metadata.csv")
+movies <-read.csv("data/movie_metadata.csv")
 
 movies <- movies %>% filter(budget<4e+08) #get rid of anomolies
 
@@ -29,15 +30,52 @@ movies <- movies %>% drop_na(ROI) #omits NA values in a column
 
 sum(is.na(movies$ROI))
 
-mean(movies$ROI)
-max(movies$ROI)
-
-# sp1 <- ggplot(movies, aes(x = grossM, y = genre_main, fill = genre_main)) + 
-#   geom_density_ridges() + 
-#   scale_x_continuous(limits = c(0, 500)) +
-#   labs(x = "Box Office Gross (USD Millions)", 
-#        y = "Main Genre")
+cat('average ROI is', mean(movies$ROI))
 
 hgp1<-ggplot(movies, aes(x=ROI)) + 
-  geom_histogram(color="black", fill="white")
+  geom_histogram(color="black", fill="white", binwidth = 500)
 hgp1
+
+ggsave("output/histogram1.png", width = 8, height = 8)
+
+#e
+count(movies, vars = ROI > 10)
+
+movies_filt <- movies %>% filter(ROI < 10) #we want/keep everything < 10 
+
+count(movies_filt, vars = ROI > 10)
+
+ggplot(data = movies_filt, aes(ROI))+
+  geom_histogram(color="black", fill="white", binwidth = 1) 
+ggsave("output/histogram2.png", width = 8, height = 8)
+
+#f
+
+average_roi_bycat <- movies_filt %>% 
+  group_by(genre_main) %>% 
+  summarize(mean(ROI))
+
+average_roi_bycat
+
+#g
+
+genre_meanROI <- average_roi_bycat$`mean(ROI)`
+genre <- average_roi_bycat$genre_main
+
+sp1 <- ggplot( data = average_roi_bycat)+ 
+  geom_point(mapping = aes(x = genre, y = genre_meanROI)) +
+  labs(x= "Genre",
+       y= "Average(ROI)",
+       title= "Average ROI by Genre")
+
+ggsave("output/scatterplot1.png", width = 10, height = 8)
+
+#h
+
+movies_filt <- group_by(movies_filt, actor_1_name)
+actors_and_mean_roi <- summarize(movies_filt, mean(ROI), profitM)
+actor_meanROI <- actors_and_mean_roi$`mean(ROI)`
+act_and_roi_slice <- actors_and_mean_roi %>% arrange(desc(actor_meanROI)) %>%
+                                             slice(1:20)  
+                                        
+act_and_roi_slice
